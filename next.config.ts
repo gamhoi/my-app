91141905652bf1d5e236f0c3ad67c90cb2231c9a
Gamhoi <gamhoi@gamhois-MacBook-Pro.local>
2025-08-13 00:16:29 -0400

    fix: set basePath/assetPrefix for GitHub Pages subpath

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@
 import type { NextConfig } from 'next';
 
 const isProd = process.env.NODE_ENV === 'production';
-// const repo = 'my-app'; // ← 改成你的仓库名；如果部署到根域名就留空并删掉下面两行
+const repo = 'my-app'; // ← 改成你的仓库名；如果部署到根域名就留空并删掉下面两行
 
 const nextConfig: NextConfig = {
   output: 'export',              // 关键：静态导出
   images: { unoptimized: true }, // GitHub Pages 无法用内置图片优化
   trailingSlash: true,           // 导出更友好（生成以 / 结尾的路径）
   ...(isProd ? {                 // 仅生产环境使用子路径
-    // basePath: `/${repo}`,
-    // assetPrefix: `/${repo}/`,
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`,
   } : {}),
   eslint: {
     ignoreDuringBuilds: true,  // ← 关键：构建时忽略 ESLint
